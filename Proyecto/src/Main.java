98a853e8c5960e8fcfd573bfc78ff65466b45d4b
Diego <sonicelerizo@uma.es>
2021-03-07 17:53:57 +0100

    Metodo privado para participantes Muestro los participantes con un método privado

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Inicio del programa.\n");
 
-        System.out.println("Miembros del curso:");
+        System.out.println("Miembros del curso\nde Git & GitHub:");
         
-        System.out.println("Diego.");
-        System.out.println("Narta.");
-        System.out.println("pedro");
-        System.out.println("Ivan.");
-        System.out.println("Manu");
-
-        System.out.println("\nFinal del programa.");
+        mostrarMiembros();
+        
+    }
+    private static void mostrarMiembros() {
+    	System.out.println("1 - Diego.");
+    	System.out.println("2 - Edu.");
+        System.out.println("3 - Iván");
+        System.out.println("4 - Javi.");
+        System.out.println("5 - Manuel");
+        System.out.println("6 - Marta");
+        System.out.println("7 - Pedro");
+        System.out.println("8 - Roberto");
     }
 }
